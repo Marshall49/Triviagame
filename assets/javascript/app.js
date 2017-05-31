f19d83aab2ed39c91365d21b95225a1dd859a008
Malcolm Strong <strong.malcolm@yahoo.com>
2017-05-30 23:13:24 -0400

    finished 90% of it

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,13 @@
 $( document ).ready(function() {
-
   var number = 25;
   var intervalId;
-
     $("#submit").on("click", stop);
     $("#remaining").on("click", run);
-
     function run () {
       intervalId = setInterval(decrement, 1000);
     }
     function decrement() {
       number--;
-
       $("#remaining").html("<h3>" + number + "</h3>");
         if (number === 0) {
         stop();
@@ -21,138 +17,91 @@ $( document ).ready(function() {
     function stop() {
       clearInterval(intervalId);
     }
-
     run();
+var userAnswer1;
+var userAnswer2;
+var userAnswer3;
+var answer1 = "1978";
+var answer2 = "Freddy Kruger";
+var answer3 = "Chucky";
+var correctAnswers = 0;
+var incorrectAnswer = 0;
+// var checkAnswers = function(){
+//   if(userAnswer1 == answer1){
+//    correctAnswers = correctAnswers +1;
+//   }else{
+//   }
+//   if(userAnswer2 == answer2){
+//     correctAnswers = correctAnswers +1;
+//   }
+//   if(userAnswer3 == answer3){
+//    correctAnswers = correctAnswers +1;
+//   }
+//   console.log(correctAnswers)
+// }
+  // var correctAnswer = 0;
+  // var incorrectAnswer = 0;
+  // var answers = [];
+$('#answers1').submit(function(){
+      //stop the form from submitting!
+    event.preventDefault();
+    userAnswer1 = $('input[name=a]:checked', '#answers1').val();
+    console.log(userAnswer1);
+    if(userAnswer1 == answer1){
+      correctAnswers = correctAnswers +1;
+        console.log(correctAnswers)
+      }
+      else(userAnswer1 != answer1);
+        incorrectAnswer = incorrectAnswer +1;
+        console.log(incorrectAnswer)
+      })
 
-  });
 
-  var correctAnswer = 0;
-  var incorrectAnswer = 0;
-  var answers = [];
-  // var questions = [{
-  // var userAnswer1 = $('input[name=a]:checked', '#answers1').html();
+
+$('#answers2').submit(function(){
+      //stop the form from submitting!
+    event.preventDefault();
+    userAnswer2 = $('input[name=b]:checked', '#answers2').val();
+    console.log(userAnswer2)
+    if(userAnswer2 == answer2){
+    correctAnswers = correctAnswers +1;
+    console.log(correctAnswers)
+    }
+      else(userAnswer2 != answer2);
+      incorrectAnswer = incorrectAnswer +1;
+      console.log(incorrectAnswer)
+    })
+
+
+$('#answers3').submit(function(){
+      //stop the form from submitting!
+    event.preventDefault();
+    userAnswer3 = $('input[name=c]:checked', '#answers3').val();
+    console.log(userAnswer3)
+    if(userAnswer3 == answer3){
+   correctAnswers = correctAnswers +1;
+   console.log(correctAnswers)
+    }
+      else(userAnswer3 != answer3);
+     incorrectAnswer = incorrectAnswer +1;
+     console.log(incorrectAnswer)
+   })
+
+   
+})
   // var userAnswer1= $("form :radio")
-  // var check = function(){
-    $("#check").click(function(event){
-      event.preventdefault();
-      var userAnswer1 = $("input[name='a']:checked").val();
-      console.log(userAnswer1);
-    });
-
-
-  // check();
-  var questions = [{
-  question: "What year did the first halloween movie come out?",
-  answers: ["1975", "1981", "1978", "1980"],
-  correctAnswer: "1978"
-}, {
-  question: "Which Character is known for the Nightmare On Elm Street Series?",
-  answers: ["Freddy Kruger", "Jason Voorhees", "Micheal Myers", "Leperchaun"],
-  correctAnswer: "Freddy Kruger"
-}, {
-  question: "Which horror movie character is a serial killer possessing a doll?",
-  answers: ["IT", "Chucky", "Preacher Kane", "Freddy Kruger"],
-  correctAnswer: "Chucky"
-}]
-
-
-  // function gameTotal(value){
-  //   if (answers.indexOf(userKey) === correctAnswer){
-  //         for (var i = 0; i < answers.length; i++){
-  //            = "_";
-  //         }
-
-
-
-
-
-
-
-
-
-  //     if (correctAnswer > targetScore){
-  //         //LOSING
-  //         alert("LOST");
-  //         losses = losses + 1;
-  //         $("#losses").html(losses);
-  //         gameStart();
-  //     }
-  //
-  //     if (targetScore == currentScore){
-  //         alert("WIN");
-  //         //WINNING
-  //         wins = wins + 1;
-  //         $("#wins").html(wins);
-  //         gameStart();
-  //     }
-  // }
-
-
-
-
-
-
-
-
-
-
-  // var game = {
-  //   questions:questions,
-  //   currentQuestion:0,
-  //   counter:countStartNumber,
-  //   correct:0,
-  //   incorrect:0,
-  //   countdown: function(){
-  //     game.counter--;
-  //     $('#choices').html(game.counter);
-  //
-  //     if (game.counter === 0){
-  //       console.log('TIME UP');
-  //       game.timeUp();
-  //     }
-  //   },
-
-
-
-
-
-
-
-
-
-
-
-
-    // var intervalId;
-    // //  When the stop button gets clicked, run the stop function.
-    // $("#button").on("click", run);
-    // //  When the resume button gets clicked, execute the run function.
-    // $("#button").on("click", run);
-    // //  The run function sets an interval
-    // //  that runs the decrement function once a second.
-    // function run () {
-    //   intervalId = setInterval(decrement, 2500);
-    // }
-    // //  The decrement function.
-    // function decrement() {
-    //   //  Decrease number by one.
-    //   number--;
-    //   //  Show the number in the #show-number tag.
-    //   $("#remaining").html("<h3>" + number + "</h3>");
-    //   //  Once number hits zero...
-    //   if (number === 0) {
-    //     //  ...run the stop function.
-    //     stop();
-    //     //  Alert the user that time is up.
-    //     alert("Time Up!");
-    //   }
-    // }
-    // //  The stop function
-    // function stop() {
-    //   //  Clears our intervalId
-    //   //  We just pass the name of the interval
-    //   //  to the clearInterval function.
-    //   clearInterval(intervalId);
-    // }
-    // //  Execute the run function.
-    // run();
+  // var questions = [{
+  //   question: "What year did the first halloween movie come out?",
+  //   answers: ["1975", "1981", "1978", "1980"],
+  //   correctAnswer: "1978"
+  //   }, {
+  //     question: "Which Character is known for the Nightmare On Elm Street Series?",
+  //     answers: ["Freddy Kruger", "Jason Voorhees", "Micheal Myers", "Leperchaun"],
+  //     correctAnswer: "Freddy Kruger"
+  //   }, {
+  //     question: "Which horror movie character is a serial killer possessing a doll?",
+  //     answers: ["IT", "Chucky", "Preacher Kane", "Freddy Kruger"],
+  //     correctAnswer: "Chucky"
+  //   }]
+// var answers= ["answer1", "answer2", "answers3"]
+// checkAnswers();
